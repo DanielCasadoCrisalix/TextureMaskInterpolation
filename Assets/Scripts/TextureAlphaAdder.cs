ff745e56ec95e8a1c9d1218e93f309433733a8c5
Daniel Casado <danielcasado@iMac-de-Daniel.local>
2018-03-16 15:51:58 +0100

    first commit

---
+++ Assets/Scripts/TextureAlphaAdder.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextureAlphaAdder : MonoBehaviour
+{
+	[SerializeField]private Texture2d smallMask;
+	private Texture2D bigMask;
+
+	void Start () {
+		
+		
+	}
+	
+}
